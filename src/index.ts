21750d5120924b711cd2110c497edec288027e38
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-06-09 10:36:50 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export function foo(s: string): object {
+type Parsed = { starting_players: string | undefined }
+
+export function foo(s: string): Parsed {
 	const lines = s.trim().split("\n").map(l => l.trim());
 
 	const initial_line = lines[0];
@@ -9,8 +11,8 @@ export function foo(s: string): object {
 		throw new Error("棋譜が {始時: で始まっています。これは2021年11月末アップデート以前の棋譜であり、まだ対応できていません。")
 	} else if (!/^\{一位色:/.test(initial_line)) {
 		throw new Error("棋譜が {一位色: で始まっていません。")
-	} 
-	
+	}
+
 	const starting_players = initial_line.match(/^\{一位色:([黒赤]+)}$/)?.[1];
 	console.log(lines);
 	return { starting_players };
